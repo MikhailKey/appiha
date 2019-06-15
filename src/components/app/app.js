65b80bf4c01d67e8361c8634a962fca39d14b6e9
Mikhail <48489639+MikhailKey@users.noreply.github.com>
2019-06-15 22:40:33 +0400

    lesson 5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,10 +10,10 @@ import PostAddForm from '../post-add-form';
 //import './app.css'
 import styled from 'styled-components';
 
+
 const AppBlock = styled.div`
     margin: 0 auto;
-    max-width: 800px;
-  
+    max-width: 800px; 
 `
 
 export default class App extends Component {
@@ -21,16 +21,17 @@ export default class App extends Component {
         super(props);
         this.state = {
             data : [
-                {label: 'Going to learn React.', important: true, id: 1},
-                {label: 'Started learning React.', important: false, id: 2},
-                {label: 'I make my life better!', important: false, id: 3},
+                {label: 'Going to learn React.', important: true, like: false, id: idGenerator()},
+                {label: 'Started learning React.', important: false, like: false, id: idGenerator()},
+                {label: 'I make my life better!', important: false, like: false, id: idGenerator()},
                 512314
             ]
         }
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
-        this.maxId = idGenerator();
-    }
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
+        }
 
     deleteItem(id) {
         this.setState(({data}) => {
@@ -47,13 +48,26 @@ export default class App extends Component {
         const newItem = {
             label: body,
             important: false,
-            id: this.maxId
+            id: this.maxId++
         }
         this.setState(({data}) => {
             const newArr = [...data, newItem];
             return {
                 data: newArr
             }
+        })
+    }
+    onToggleImportant (id) {
+        console.log(`important ${id}`);
+    }
+    onToggleLiked (id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+
+            const newArr = [...data.slice(0, index), newItem,...data.slice(index + 1)];
         })
     }
 
@@ -65,12 +79,15 @@ export default class App extends Component {
                     <SearchPanel />
                     <PostStatusFilter />
                 </div>
-                <PostList posts={this.state.data}
-                    onDelete={this.deleteItem} />
+                <PostList 
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleLiked = {this.onToggleLiked} />
+
                 <PostAddForm
                     onAdd={this.addItem}/>
-            </AppBlock>
-            
+            </AppBlock>           
         ) 
     }
     }
